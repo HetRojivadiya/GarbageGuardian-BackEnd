1c99a3394afd73787c57f451dcb6f042c1ca5d44
Het Rojivadiya <het@Hets-MacBook-Air.local>
2024-10-18 22:14:15 +0530

    fifth

--- controllers/authController.js
+++ controllers/authController.js
@@ -24,6 +24,7 @@ exports.signup = async (req, res) => {
 
 // Login
 exports.login = async (req, res) => {
+
   const { email, password } = req.body;
   try {
     const user = await User.findOne({ email });
@@ -32,7 +33,7 @@ exports.login = async (req, res) => {
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(400).json({ message: 'Invalid Credentials' });
 
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+    const token = jwt.sign({ userId: user._id , userName : user.name}, process.env.JWT_SECRET, {
       expiresIn: '1h',
     });
     res.json({ token });
